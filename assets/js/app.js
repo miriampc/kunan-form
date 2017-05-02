23e83628220ffa041d1a98a618acbbc7346a4c62
Miriam Peralta <miiriampc@gmail.com>
2017-05-01 23:45:22 -0500

    agregando funciones para checks

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+var regNacimiento =document.getElementById('reg-nacimiento');
+var regResidencia =document.getElementById('reg-residencia');
+var regiones =document.getElementById('regiones');
+var sector =document.getElementById('sector');
+var fuente =document.getElementById('fuente');
+var montos =document.getElementById('montos');
+var medio =document.getElementById('medio');
+
+regionesData.forEach(function(e){
+  regNacimiento.appendChild(options(e));
+  regResidencia.appendChild(options(e));
+  regiones.appendChild(checkboxs(e));
+});
+
+sectores.forEach(function(e){
+  sector.appendChild(checkboxs(e));
+});
+
+fuentes.forEach(function(e){
+  fuente.appendChild(checkboxs(e));
+});
+
+fondos.forEach(function(e){
+  montos.appendChild(checkboxs(e));
+});
+
+medios.forEach(function(e){
+  medio.appendChild(checkboxs(e));
+});
+
+function options(region){
+  var option=document.createElement('option');
+  option.appendChild(document.createTextNode(region));
+  return option;
+}
+
+function checkboxs(texto){
+  var p=document.createElement('p');
+  var input=document.createElement('input');
+  p.setAttribute("class","bold");
+  input.type="button";
+  input.classList.add("checkbox","checked");
+  p.appendChild(input);
+  p.appendChild(document.createTextNode(" "+texto));
+  return p;
+}
+
+document.getElementsByClassName('checkbox').onclick=function(e){
+  // e.preventDefault();
+  console.log("asd");
+  document.getElementsByClassName('checkbox')[0].style.background="black";
+}
---
+++ assets/js/data.js
@@ -0,0 +1,16 @@
+var regionesData=["Amazonas","Áncash","Apurímac","Arequipa","Ayacucho","Cajamarca","Callao","Cuzco",
+              "Huancavelica","Huánuco","Ica","Junín","La Libertad","Lambayeque","Lima","Loreto",
+              "Madre de Dios","Moquegua","Pasco","Piura","Puno","San Martín","Tacna","Tumbes","Ucayali"];
+
+var sectores=["Género","Educación","Inclusión social","Medio ambiente","Salud","Institucionalidad",
+              "Turismo","Cultura","Arte","Diseño","Gastronomía","Valores","Deporte","Transporte"];
+
+var fuentes=["He ganado uno o varios grants y/o capitales semilla","Cuento con inversionistas privados",
+             "Hago campañas de Crowdfunding","He sido o estoy siendo incubado por una organización",
+             "Una parte o todo es por la venta de mis productos o servicios","Otros"];
+
+var fondos=["<= S/. 100,000","S/. 100,001 – 200,000","S/. 200,001 – 300,000","S/. 300,001 – 400,000",
+            "S/. 400,001 – 500,000",">=S/. 500,001"];
+
+var medios=["Amigo","Web","Redes sociales","Radio/ TV","Periódico/Revista",
+           "Charla","Organización","Universidad/Instituto","Otro"];
